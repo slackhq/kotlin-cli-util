b9fe0c911823d88911313d061683f8341d4aa28a
Zac Sweers <pandanomic@gmail.com>
2023-06-11 16:12:45 -0400

    Switch to Path for projectDirOption

--- src/main/kotlin/slack/cli/CliktExtensions.kt
+++ src/main/kotlin/slack/cli/CliktExtensions.kt
@@ -27,8 +27,8 @@ import com.github.ajalt.clikt.parameters.options.OptionDelegate
 import com.github.ajalt.clikt.parameters.options.defaultLazy
 import com.github.ajalt.clikt.parameters.options.flag
 import com.github.ajalt.clikt.parameters.options.option
-import com.github.ajalt.clikt.parameters.types.file
-import java.io.File
+import com.github.ajalt.clikt.parameters.types.path
+import java.nio.file.Path
 import java.nio.file.Paths
 
 /** A dry run option for [clikt commands][CliktCommand]. */
@@ -41,7 +41,7 @@ public fun CliktCommand.dryRunOption(
 public fun CliktCommand.projectDirOption(
   vararg names: String = arrayOf("--project-dir"),
   help: String = "The project directory. Defaults to the current working directory."
-): OptionDelegate<File> =
-  option(names = names, help = help).file(mustExist = true, canBeFile = false).defaultLazy {
-    Paths.get("").toFile().canonicalFile
+): OptionDelegate<Path> =
+  option(names = names, help = help).path(mustExist = true, canBeFile = false).defaultLazy {
+    Paths.get("").toAbsolutePath()
   }
